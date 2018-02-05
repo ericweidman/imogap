7eac3349ebf0ef5d0b5dbddcfef24ceb4da7612b
Eric Weidman <ericweidman@gmail.com>
2018-02-04 17:07:33 -0800

    Initial Import

---
+++ imogap/src/main/java/org/imogap/ImogapApplication.java
@@ -0,0 +1,12 @@
+package org.imogap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImogapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImogapApplication.class, args);
+	}
+}
